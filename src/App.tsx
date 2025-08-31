5c22cb33723e008577560d1074e56e875a49e32a
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-09-01 01:07:38 +0200

    add keyboard piano

--- src/App.tsx
+++ src/App.tsx
@@ -57,6 +57,108 @@ function App() {
             Note C 220
           </button>
         </div>
+        <div className='relative flex'>
+          {/* White keys */}
+          <button
+            onMouseDown={() => playNote(261.63)} // C4
+            onMouseUp={stopNote}
+            className='bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-900 font-bold'
+          >
+            C
+          </button>
+          <button
+            onMouseDown={() => playNote(293.66)} // D4
+            onMouseUp={stopNote}
+            className='bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-900 font-bold'
+          >
+            D
+          </button>
+          <button
+            onMouseDown={() => playNote(329.63)} // E4
+            onMouseUp={stopNote}
+            className='bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-900 font-bold'
+          >
+            E
+          </button>
+          <button
+            onMouseDown={() => playNote(349.23)} // F4
+            onMouseUp={stopNote}
+            className='bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-900 font-bold'
+          >
+            F
+          </button>
+          <button
+            onMouseDown={() => playNote(392.0)} // G4
+            onMouseUp={stopNote}
+            className='bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-900 font-bold'
+          >
+            G
+          </button>
+          <button
+            onMouseDown={() => playNote(440.0)} // A4
+            onMouseUp={stopNote}
+            className='bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-900 font-bold'
+          >
+            A
+          </button>
+          <button
+            onMouseDown={() => playNote(493.88)} // B4
+            onMouseUp={stopNote}
+            className='bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-900 font-bold'
+          >
+            B
+          </button>
+          <button
+            onMouseDown={() => playNote(523.25)} // C5
+            onMouseUp={stopNote}
+            className='bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-900 font-bold'
+          >
+            C
+          </button>
+
+          {/* Black keys */}
+          <button
+            onMouseDown={() => playNote(277.18)} // C#4/Db4
+            onMouseUp={stopNote}
+            className='bg-gray-800 hover:bg-gray-700 active:bg-gray-600 rounded-md w-8 h-28 absolute left-[48px] top-0 z-10 text-white font-bold'
+            style={{ marginLeft: '-16px' }}
+          >
+            C#
+          </button>
+          <button
+            onMouseDown={() => playNote(311.13)} // D#4/Eb4
+            onMouseUp={stopNote}
+            className='bg-gray-800 hover:bg-gray-700 active:bg-gray-600 rounded-md w-8 h-28 absolute left-[98px] top-0 z-10 text-white font-bold'
+            style={{ marginLeft: '-16px' }}
+          >
+            D#
+          </button>
+          {/* No black key between E and F */}
+          <button
+            onMouseDown={() => playNote(369.99)} // F#4/Gb4
+            onMouseUp={stopNote}
+            className='bg-gray-800 hover:bg-gray-700 active:bg-gray-600 rounded-md w-8 h-28 absolute left-[195px] top-0 z-10 text-white font-bold'
+            style={{ marginLeft: '-16px' }}
+          >
+            F#
+          </button>
+          <button
+            onMouseDown={() => playNote(415.3)} // G#4/Ab4
+            onMouseUp={stopNote}
+            className='bg-gray-800 hover:bg-gray-700 active:bg-gray-600 rounded-md w-8 h-28 absolute left-[245px] top-0 z-10 text-white font-bold'
+            style={{ marginLeft: '-16px' }}
+          >
+            G#
+          </button>
+          <button
+            onMouseDown={() => playNote(466.16)} // A#4/Bb4
+            onMouseUp={stopNote}
+            className='bg-gray-800 hover:bg-gray-700 active:bg-gray-600 rounded-md w-8 h-28 absolute left-[295px] top-0 z-10 text-white font-bold'
+            style={{ marginLeft: '-16px' }}
+          >
+            A#
+          </button>
+        </div>
       </main>
     </>
   );
