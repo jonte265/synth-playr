cc74923dc31e0bf8aff8384eff8d44f7769b70b6
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-09-01 20:10:50 +0200

    map out keys instead

--- src/App.tsx
+++ src/App.tsx
@@ -36,6 +36,25 @@ function App() {
     oscillatorRef.current = null;
   }
 
+  const whiteKeys = [
+    { note: 'C', freq: 261.63 },
+    { note: 'D', freq: 293.66 },
+    { note: 'E', freq: 329.63 },
+    { note: 'F', freq: 349.23 },
+    { note: 'G', freq: 392.0 },
+    { note: 'A', freq: 440.0 },
+    { note: 'B', freq: 493.88 },
+    { note: 'C', freq: 523.25 },
+  ];
+
+  const blackKeys = [
+    { note: 'C#', freq: 277.18, left: 48 },
+    { note: 'D#', freq: 311.13, left: 98 },
+    { note: 'F#', freq: 369.99, left: 195 },
+    { note: 'G#', freq: 415.3, left: 245 },
+    { note: 'A#', freq: 466.16, left: 295 },
+  ];
+
   return (
     <>
       <Header />
@@ -56,108 +75,30 @@ function App() {
             Note C 220
           </button>
         </div>
-        <div className='relative flex'>
-          {/* White keys */}
-          <button
-            onMouseDown={() => playNote(261.63)} // C4
-            onMouseUp={stopNote}
-            className=' flex justify-center items-end pb-4 bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-500 font-bold'
-          >
-            C
-          </button>
-          <button
-            onMouseDown={() => playNote(293.66)} // D4
-            onMouseUp={stopNote}
-            className='flex justify-center items-end pb-4
- bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-500 font-bold'
-          >
-            D
-          </button>
-          <button
-            onMouseDown={() => playNote(329.63)} // E4
-            onMouseUp={stopNote}
-            className='flex justify-center items-end pb-4 bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-500 font-bold'
-          >
-            E
-          </button>
-          <button
-            onMouseDown={() => playNote(349.23)} // F4
-            onMouseUp={stopNote}
-            className='flex justify-center items-end pb-4 bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-500 font-bold'
-          >
-            F
-          </button>
-          <button
-            onMouseDown={() => playNote(392.0)} // G4
-            onMouseUp={stopNote}
-            className='flex justify-center items-end pb-4 bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-500 font-bold'
-          >
-            G
-          </button>
-          <button
-            onMouseDown={() => playNote(440.0)} // A4
-            onMouseUp={stopNote}
-            className='flex justify-center items-end pb-4 bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-500 font-bold'
-          >
-            A
-          </button>
-          <button
-            onMouseDown={() => playNote(493.88)} // B4
-            onMouseUp={stopNote}
-            className='flex justify-center items-end pb-4 bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-500 font-bold'
-          >
-            B
-          </button>
-          <button
-            onMouseDown={() => playNote(523.25)} // C5
-            onMouseUp={stopNote}
-            className=' bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-500 font-bold'
-          >
-            C
-          </button>
 
-          {/* Black keys */}
-          <button
-            onMouseDown={() => playNote(277.18)} // C#4/Db4
-            onMouseUp={stopNote}
-            className='bg-gray-800 hover:bg-gray-700 active:bg-gray-600 rounded-md w-8 h-28 absolute left-[48px] top-0 z-10 text-gray-300 font-bold'
-            style={{ marginLeft: '-16px' }}
-          >
-            C#
-          </button>
-          <button
-            onMouseDown={() => playNote(311.13)} // D#4/Eb4
-            onMouseUp={stopNote}
-            className='bg-gray-800 hover:bg-gray-700 active:bg-gray-600 rounded-md w-8 h-28 absolute left-[98px] top-0 z-10 text-gray-300 font-bold'
-            style={{ marginLeft: '-16px' }}
-          >
-            D#
-          </button>
-          {/* No black key between E and F */}
-          <button
-            onMouseDown={() => playNote(369.99)} // F#4/Gb4
-            onMouseUp={stopNote}
-            className='bg-gray-800 hover:bg-gray-700 active:bg-gray-600 rounded-md w-8 h-28 absolute left-[195px] top-0 z-10 text-gray-300 font-bold'
-            style={{ marginLeft: '-16px' }}
-          >
-            F#
-          </button>
-          <button
-            onMouseDown={() => playNote(415.3)} // G#4/Ab4
-            onMouseUp={stopNote}
-            className='bg-gray-800 hover:bg-gray-700 active:bg-gray-600 rounded-md w-8 h-28 absolute left-[245px] top-0 z-10 text-gray-300 font-bold'
-            style={{ marginLeft: '-16px' }}
-          >
-            G#
-          </button>
-          <button
-            onMouseDown={() => playNote(466.16)} // A#4/Bb4
-            onMouseUp={stopNote}
-            className='bg-gray-800 hover:bg-gray-700 active:bg-gray-600 rounded-md w-8 h-28 absolute left-[295px] top-0 z-10 text-gray-300 font-bold'
-            style={{ marginLeft: '-16px' }}
-          >
-            A#
-          </button>
+        <div className='relative flex'>
+          {whiteKeys.map((key, index) => (
+            <button
+              key={`white-${key.note}-${index}`}
+              onMouseDown={() => playNote(key.freq)}
+              onMouseUp={stopNote}
+              className='flex justify-center items-end pb-4 min-h-45 bg-gray-300 hover:bg-gray-400  active:text-gray-300  active:bg-gray-500 rounded-xl px-4 py-20 border border-gray-400 relative z-0 text-gray-500 font-bold'
+            >
+              {key.note}
+            </button>
+          ))}
+
+          {blackKeys.map((key, index) => (
+            <button
+              key={`black-${key.note}-${index}`}
+              onMouseDown={() => playNote(key.freq)}
+              onMouseUp={stopNote}
+              className='bg-gray-800 hover:bg-gray-700 active:bg-gray-600 rounded-md w-8 h-28 absolute top-0 z-10 text-gray-300 font-bold'
+              style={{ marginLeft: '-16px', left: `${key.left}px` }}
+            >
+              {key.note}
+            </button>
+          ))}
         </div>
       </main>
     </>
