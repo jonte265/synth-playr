1cc972df40f85a45c9c9417b31720495d4e538f4
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-09-01 21:02:47 +0200

    add notes to computer keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -61,8 +61,53 @@ function App() {
         return;
       }
 
-      console.log(`Key down: ${event.key}`);
-      playNote(440);
+      switch (event.key) {
+        case 'a':
+          playNote(261.63); // C4
+          break;
+        case 'w':
+          playNote(277.18); // C#4
+          break;
+        case 's':
+          playNote(293.66); // D4
+          break;
+        case 'e':
+          playNote(311.13); // D#4
+          break;
+        case 'd':
+          playNote(329.63); // E4
+          break;
+        case 'f':
+          playNote(349.23); // F4
+          break;
+        case 't':
+          playNote(369.99); // F#4
+          break;
+        case 'g':
+          playNote(392.0); // G4
+          break;
+        case 'y':
+          playNote(415.3); // G#4
+          break;
+        case 'h':
+          playNote(440.0); // A4
+          break;
+        case 'u':
+          playNote(466.16); // A#4
+          break;
+        case 'j':
+          playNote(493.88); // B4
+          break;
+        case 'k':
+          playNote(523.25); // C5
+          break;
+        case 'o':
+          playNote(554.37); // C#5
+          break;
+        case 'l':
+          playNote(587.33); // D5
+          break;
+      }
     }
     function handleKeyup(event) {
       console.log(`Key up: ${event.key}`);
